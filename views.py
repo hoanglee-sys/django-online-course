5ea0fac5eaa7333558cea318a688c21b26403a3e
hoanglee-sys <vuminhh291@gmail.com>
2026-07-12 11:16:00 +0700

    Update views.py

--- views.py
+++ views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Course, Enrollment, Submission
 
 def submit(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    if request.method == 'POST':
+        enrollment = Enrollment.objects.get(user=request.user, course=course)
+        submission = Submission.objects.create(enrollment=enrollment)
+        return redirect('show_exam_result', course_id=course_id)
     return render(request, 'submit.html')
 
 def show_exam_result(request, course_id):
