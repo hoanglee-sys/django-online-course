5b01591a3f8d6ce9d6e9f1d30d4b0e05c415daed
hoanglee-sys <vuminhh291@gmail.com>
2026-07-12 11:15:23 +0700

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
+class Course(models.Model): pass
+class Lesson(models.Model): pass
+class Instructor(models.Model): pass
+class Learner(models.Model): pass
+class Enrollment(models.Model): pass
+
 class Question(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     content = models.CharField(max_length=200)
 
 class Choice(models.Model):
@@ -9,4 +16,5 @@ class Choice(models.Model):
     is_correct = models.BooleanField(default=False)
 
 class Submission(models.Model):
-    score = models.FloatField()
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
